9e1c2b63426288a1b09392597177f0f1a2fa7ce0
miacruz21 <118496929+miacruz21@users.noreply.github.com>
2024-12-02 19:56:15 +0800

    port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:21922"
+  origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 21922;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
